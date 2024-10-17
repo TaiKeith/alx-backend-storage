489925094467c27292a082f31c6072092a19a94e
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-17 13:46:44 +0300

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 -- Attributes for computing 'lifespan': 'formed' and 'split'
 SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style)
+WHERE style LIKE '%Glam rock'
 ORDER BY lifespan DESC;
