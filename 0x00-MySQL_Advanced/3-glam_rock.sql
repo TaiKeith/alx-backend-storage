e9a82581ee234803516f6efda741dc830da29d16
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-17 13:53:19 +0300

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with 'Glam rock' as their main style, ranked by their longevity
 -- Column names: 'band_name' and 'lifespan' (in years until 2022, use 2022 instead of YEAR(CURDATE())
 -- Attributes for computing 'lifespan': 'formed' and 'split'
-SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
