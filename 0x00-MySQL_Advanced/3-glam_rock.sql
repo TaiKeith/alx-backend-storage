0c01a8f1bfcd1b141378f428d10e7e4f63881831
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-18 00:25:12 +0300

    Updated code

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,7 @@
 -- Column names: 'band_name' and 'lifespan'
 -- (in years until 2022, use 2022 instead of YEAR(CURDATE())
 -- Attributes for computing 'lifespan': 'formed' and 'split'
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
