f7961149e7826753c14a0842ce79fc9c1719054a
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-17 14:03:00 +0300

    Old school band fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -5,6 +5,5 @@
 -- Attributes for computing 'lifespan': 'formed' and 'split'
 SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
---WHERE style LIKE 'Glam rock'
-WHERE FIND_IN_SET('Glam rock', STYLE)
+WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
