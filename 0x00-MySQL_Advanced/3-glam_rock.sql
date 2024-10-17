b12a92c63da701dad6eb9d6e0175e3e33c204c14
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-17 20:41:43 +0300

    Fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,6 @@
--- Lists all bands with 'Glam rock' as their main style, ranked by their
--- longevity.
--- Column names: 'band_name' and 'lifespan'
--- (in years until 2022, use 2022 instead of YEAR(CURDATE())
--- Attributes for computing 'lifespan': 'formed' and 'split'
-SELECT name AS band_name,
-CASE WHEN split IS NULL THEN 2022 - formed ELSE split - formed
-END AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
